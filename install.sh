8b1694496840cca98ff911988724ded0de6eb54f
Will Munslow <will@subterrane.com>
2024-01-14 21:04:23 -0800

    fix raw url

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 mkdir ~/jrnl
-curl -o ~/jrnl/jrnl.sh https://github.com/Subterrane/jrnl/blob/main/jrnl.sh
+curl -o ~/jrnl/jrnl.sh https://raw.githubusercontent.com/Subterrane/jrnl/main/jrnl.sh?token=GHSAT0AAAAAACM3I67BYUFZ7E5PNGKLT2FQZNEZLFA
 chmod +x ~/jrnl/jrnl.sh
 echo "alias jrnl='~/jrnl/jrnl.sh'" >> ~/.bashrc
 source ~/.bashrc
