655dfd1f689b6f409a641552aca2062b00d56c0a
Will Munslow <will@subterrane.com>
2024-01-14 21:45:32 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ mkdir ~/jrnl > /dev/null 2>&1
 curl -s -o ~/jrnl/jrnl.sh https://raw.githubusercontent.com/Subterrane/jrnl/main/jrnl.sh
 chmod +x ~/jrnl/jrnl.sh
 curl -s -o ~/jrnl/start.md https://raw.githubusercontent.com/Subterrane/jrnl/main/start.md
-alias jrnl > /dev/null 2>&1 && echo "jrnl is already set as an alias" || echo "alias jrnl='~/jrnl/jrnl.sh'" >> ~/.zshrc
+alias jrnl >/dev/null 2>&1 && echo "jrnl is already set as an alias" || echo "alias jrnl='~/jrnl/jrnl.sh'" >> ~/.zshrc
 source ~/.zshrc
