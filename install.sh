d2af6614ddf2c1a8b91e0d12c63df6a806e8ae1d
Will Munslow <will@subterrane.com>
2024-01-14 21:30:36 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir ~/jrnl
-curl -o ~/jrnl/jrnl.sh https://raw.githubusercontent.com/Subterrane/jrnl/main/jrnl.sh
+curl -so ~/jrnl/jrnl.sh https://raw.githubusercontent.com/Subterrane/jrnl/main/jrnl.sh
 chmod +x ~/jrnl/jrnl.sh
-curl -o ~/jrnl/start.md https://raw.githubusercontent.com/Subterrane/jrnl/main/start.md
+curl -so ~/jrnl/start.md https://raw.githubusercontent.com/Subterrane/jrnl/main/start.md
 echo "alias jrnl='~/jrnl/jrnl.sh'" >> ~/.zshrc
 source ~/.zshrc
